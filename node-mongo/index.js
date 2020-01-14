83b977feb26c9e3b626fca6f3a264940ed7ec522
akanksha7 <akanksha@tpix.in>
2020-01-14 16:50:45 +0530

    Week 2 Node and MongoDB   Crud Operations

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -7,22 +7,45 @@ MongoClient.connect(url,(err,client)=>{
     assert.equal(err,null);
     console.log('Connected correctly to server');
     const db = client.db(dbname);
-    const collection = db.collection("dishes");
-    collection.insertOne({"name": "Uthapizza", "description": "test"}, (err,result)=>{
-          assert.equal(err,null);
-          console.log("After Insert: \n");
-          console.log(result.ops);
-          collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
-            console.log("Found:\n");
-            console.log(docs);
-          });
-          db.dropCollection("dishes", (err, result) => {
-            assert.equal(err,null);
+    // const collection = db.collection("dishes");
+    // collection.insertOne({"name": "Uthapizza", "description": "test"}, (err,result)=>{
+    //       assert.equal(err,null);
+    //       console.log("After Insert: \n");
+    //       console.log(result.ops);
+    //       collection.find({}).toArray((err, docs) => {
+    //         assert.equal(err,null);
+    //         console.log("Found:\n");
+    //         console.log(docs);
+    //       });
+    //       db.dropCollection("dishes", (err, result) => {
+    //         assert.equal(err,null);
+
+    //         client.close();
+    //     });
+
+    // });
+
+
+  dboper.insertDocument(db,{"name": "Uttapizza","description": "test"},"dishes",(result)=>{
+    console.log("Insert Document:\n", result.ops);
+    dboper.findDocument(db,"dishes",(docs)=>{
+        console.log("Found Documents:\n", docs);
+    dboper.updateDocument(db,{ name: "Vadonut" } ,{ description: "Updated Test" },"dishes", (result) => {
+        console.log("Updated Document:\n", result.result);
+     dboper.findDocument(db,"dishes",(docs)=>{
+         console.log("Found Document:\n",docs);
+         db.dropCollection("dishes", (result) => {
+            console.log("Dropped Collection: ", result);
 
             client.close();
         });
+     });
+
 
     });
+    });
+  });
+
+
 
 });
